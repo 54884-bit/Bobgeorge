86a7573b9ca5d3dac8694d0f71e3968e858ddca2
54884-bit <54884@cv.k12.ca.us>
2025-09-29 12:32:07 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,42 +1,114 @@
+import pygame
+import sys 
 import random
 
-race_breakdown = random.randint(1,2)
-winning_horse = random.randint(1,5)
-horse_chosen = random.randint(1,5)
-money_bet = random.randint(1,20)
-money = 100
+pygame.init()
 
+SW, SH = 800, 800
 
-print(f"money bet : {money_bet}")
-print(f"horse chosen : {horse_chosen}")
+BLOCK_SIZE = 50
+FONT = pygame.font.Font("font.ttf", BLOCK_SIZE*2)
 
-if race_breakdown < 2 and race_breakdown > 0:
-    print(f"its a really close race, but horse number {winning_horse} wins!")
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Snake!")
+clock = pygame.time.Clock()
 
-if race_breakdown < 3 and race_breakdown > 1:
-    print(f"its a no brainer as all the other horses are smoked and horse number {winning_horse} wins!")
-
-print(f"winning horse : {winning_horse}")
-
-if horse_chosen == winning_horse:
-    if winning_horse is money_bet:
-            Double_money = money_bet + money_bet
-            print(f"double double! you win {Double_money}")
-            money_made = Double_money
-    else:
-        print(f"you won ${money_bet}")
-        money_made = money + money_bet
+class Snake:
+    def __init__(self):
+        self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
+        self.xdir = 1
+        self.ydir = 0
+        self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
+        self.body = [pygame.Rect(self.x-BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
+        self.dead = False
     
+    def update(self):
+        global apple
+        
+        for square in self.body:
+            if self.head.x == square.x and self.head.y == square.y:
+                self.dead = True
+            if self.head.x not in range(0, SW) or self.head.y not in range(0, SH):
+                self.dead = True
+        
+        if self.dead:
+            self.x, self.y = BLOCK_SIZE, BLOCK_SIZE
+            self.head = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
+            self.body = [pygame.Rect(self.x-BLOCK_SIZE, self.y, BLOCK_SIZE, BLOCK_SIZE)]
+            self.xdir = 1
+            self.ydir = 0
+            self.dead = False
+            apple = Apple()
+        
+        self.body.append(self.head)
+        for i in range(len(self.body)-1):
+            self.body[i].x, self.body[i].y = self.body[i+1].x, self.body[i+1].y
+        self.head.x += self.xdir * BLOCK_SIZE
+        self.head.y += self.ydir * BLOCK_SIZE
+        self.body.remove(self.head)
+
+class Apple:
+    def __init__(self):
+        self.x = int(random.randint(0, SW)/BLOCK_SIZE) * BLOCK_SIZE
+        self.y = int(random.randint(0, SH)/BLOCK_SIZE) * BLOCK_SIZE
+        self.rect = pygame.Rect(self.x, self.y, BLOCK_SIZE, BLOCK_SIZE)
     
-else:
-    if money_bet == winning_horse:
-        Double_lost = money_bet + money_bet
-        print(f"Double double! you lost ${Double_lost}")
-        money_lost = money - Double_lost
-    else:
-        print(f"sorry, but you lost ${money_bet}")
-        money_lost = money - money_bet
-    print("press run to lose more money")
-    print(f"you now have ${money_lost}")
+    def update(self):
+        pygame.draw.rect(screen, "orange", self.rect)
 
+def drawGrid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
+score = FONT.render("1", True, "white")
+score_rect = score.get_rect(center=(SW/2, SH/20))
+
+drawGrid()
+
+snake = Snake()
+
+apple = Apple()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                snake.ydir = 1
+                snake.xdir = 0
+            elif event.key == pygame.K_UP:
+                snake.ydir = -1
+                snake.xdir = 0
+            elif event.key == pygame.K_RIGHT:
+                snake.ydir = 0
+                snake.xdir = 1
+            elif event.key == pygame.K_LEFT:
+                snake.ydir = 0
+                snake.xdir = -1
+
+    snake.update()
+    
+    screen.fill('black')
+    drawGrid()
+
+    apple.update()
+
+    score = FONT.render(f"{len(snake.body) + 1}", True, "white")
+
+    pygame.draw.rect(screen, "green", snake.head)
+
+    for square in snake.body:
+        pygame.draw.rect(screen, "green", square)
+
+    screen.blit(score, score_rect)
+
+    if snake.head.x == apple.x and snake.head.y == apple.y:
+        snake.body.append(pygame.Rect(square.x, square.y, BLOCK_SIZE, BLOCK_SIZE))
+        apple = Apple()
+
+    pygame.display.update()
+    clock.tick(5)
